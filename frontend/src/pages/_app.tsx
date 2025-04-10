e2face6d23e7663190e1af1f3ef9e0b612da1820
PangKiYeon <pdrkeamy@gmail.com>
2025-04-10 17:26:30 +0900

    FEAT : favicon 추가

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,14 +1,18 @@
 import '@/styles/globals.css';
+import '@/styles/swiper.css';
 import type { AppProps } from 'next/app';
-import { hanna } from '@/styles/font';
+import Head from 'next/head';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
-
 export default function App({ Component, pageProps }: AppProps) {
+    const queryClient = new QueryClient();
+
     return (
         <QueryClientProvider client={queryClient}>
-            <main className={`${hanna.variable} font-hanna`}>
+            <Head>
+                <link rel="icon" href="/favicon.png" />
+            </Head>
+            <main>
                 <Component {...pageProps} />
             </main>
         </QueryClientProvider>
