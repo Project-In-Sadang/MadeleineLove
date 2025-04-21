e4367a225533f0fbca931bf8b70fa8e41859712d
PangKiYeon <pdrkeamy@gmail.com>
2025-04-21 13:20:52 +0900

    FEAT : token에 따른 경로 리다이렉션 추가

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,10 +2,22 @@ import '@/styles/globals.css';
 import '@/styles/swiper.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { usePathname } from 'next/navigation';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
     const queryClient = new QueryClient();
+    const pathname = usePathname();
+
+    useEffect(() => {
+        const token = localStorage.getItem('access_token');
+        if (!token && pathname !== '/' && pathname !== '/allHeart') {
+            window.location.href = '/';
+        } else if (token && pathname === '/') {
+            window.location.href = '/main';
+        }
+    }, [pathname]);
 
     return (
         <QueryClientProvider client={queryClient}>
